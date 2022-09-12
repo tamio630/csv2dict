003a0e710d28b6aba230011847d8ccbad933d03a
tamio630 <57208347+tamio630@users.noreply.github.com>
2022-09-12 22:08:32 +0900

    Add files via upload

---
+++ csv2dict.py
@@ -0,0 +1,18 @@
+import csv
+
+def Csv2Dict(fname):
+
+    with open(fname, "r") as f:
+        reader = csv.reader(f)
+
+        csv_dict = {}
+    
+        for row in reader:
+            index = row[0]
+            url = row[1]
+            csv_dict[index] = url
+
+    return csv_dict
+
+if __name__ == "__main__":
+    print(Csv2Dict("ryousei.csv"))
